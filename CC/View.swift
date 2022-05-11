56c72b5dff901566e147898b3c2614485a9a75a4
Red <seryprk@gmail.com>
2022-05-12 05:03:49 +0900

    feat: 컴포지션 레이아웃 코드 추가

--- CC/View.swift
+++ CC/View.swift
@@ -10,18 +10,19 @@ import UIKit
 class View: UIView {
     // 세그먼트 생성
     let segment: UISegmentedControl = {
-        let control = UISegmentedControl(items: ["firstView", "secondView"])
+        let control = UISegmentedControl(items: ["그리드레이아웃", "라인레이아웃", "그리드컴포지션", "라인컴포지션", "사진컴포지션"])
         control.translatesAutoresizingMaskIntoConstraints = false
         return control
       }()
     //콜렉션 뷰 생성
     lazy var firstCollectionView: UICollectionView = {
-        let view = UICollectionView(frame: .zero, collectionViewLayout: firstLayout)
+        let view = UICollectionView(frame: .zero, collectionViewLayout: gridLayout)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+    // MARK: 플로우 레이아웃
     // 첫번쨰 스타일 레이아웃 지정
-    lazy var firstLayout: UICollectionViewFlowLayout = {
+    lazy var gridLayout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.sectionInset = UIEdgeInsets(top: 20.0, left: 10.0, bottom: 10.0, right: 10.0)
@@ -30,13 +31,96 @@ class View: UIView {
         return layout
     }()
     // 두번째 스타일 레이아웃 지정
-    lazy var secoundLayout: UICollectionViewFlowLayout = {
+    lazy var lineLayout: UICollectionViewFlowLayout = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.sectionInset = UIEdgeInsets(top: 20.0, left: 10.0, bottom: 10.0, right: 10.0)
         
         let availableWidth = self.layer.bounds.width
         layout.itemSize = CGSize(width: availableWidth - 20, height: 100)
+        return layout
+    }()
+    
+    // MARK: 컴포지션 레이아웃
+    // 첫번째 스타일 레이아웃 컴포지션으로 생성하기
+    lazy var gridLayoutComposition: UICollectionViewCompositionalLayout = {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/3),
+                                                 heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
+          
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .absolute(44))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+          
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }()
+    // 두번째 스타일 레이아웃 컴포지션으로 생성하기
+    lazy var lineLayoutComposition: UICollectionViewCompositionalLayout = {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                 heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
+          
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .absolute(44))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                             subitems: [item])
+          
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }()
+    // 사진 스타일 레이아웃 컴포지션으로 생성하기
+    lazy var pictureLayoutComposition: UICollectionViewCompositionalLayout = {
+        // 왼쪽 큰셀 설정
+        let mainItem = NSCollectionLayoutItem(
+          layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(2/3),
+            heightDimension: .fractionalHeight(1.0)))
+        
+        mainItem.contentInsets = NSDirectionalEdgeInsets(
+          top: 2,
+          leading: 2,
+          bottom: 2,
+          trailing: 2)
+        // 작은 오른쪽 셀 설정
+        let pairItem = NSCollectionLayoutItem(
+          layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(0.5)))
+        
+        pairItem.contentInsets = NSDirectionalEdgeInsets(
+          top: 2,
+          leading: 2,
+          bottom: 2,
+          trailing: 2)
+        // 작은 오른쪽 셀그룹 만들어서 2개 반복
+        let trailingGroup = NSCollectionLayoutGroup.vertical(
+          layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1/3),
+            heightDimension: .fractionalHeight(1.0)),
+          subitem: pairItem,
+          count: 2)
+        // 큰셀 작은셀 합치기
+        let mainWithPairGroup = NSCollectionLayoutGroup.horizontal(
+          layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(4/9)),
+          subitems: [mainItem, trailingGroup])
+          
+        let section = NSCollectionLayoutSection(group: mainWithPairGroup)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
         return layout
     }()
     // init 이 있어야 한다. 어디든...
@@ -73,11 +157,15 @@ class View: UIView {
     @objc func switchSegment(segmentControll: UISegmentedControl) {
         switch segmentControll.selectedSegmentIndex {
         case 0:
-            firstCollectionView.collectionViewLayout = firstLayout
-            print("haha")
+            firstCollectionView.collectionViewLayout = gridLayout
         case 1:
-            firstCollectionView.collectionViewLayout = secoundLayout
-            print("leelee")
+            firstCollectionView.collectionViewLayout = lineLayout
+        case 2:
+            firstCollectionView.collectionViewLayout = gridLayoutComposition
+        case 3:
+            firstCollectionView.collectionViewLayout = lineLayoutComposition
+        case 4:
+            firstCollectionView.collectionViewLayout = pictureLayoutComposition
         default:
             print("없음")
         }
